016356e055c02706ce200c926b82a7f43697d1c6
Luke Memet <lukememet@gmail.com>
2026-05-02 23:40:24 -0400

    Revert "feat(apple-spm): Add Swift Package manifests"
    This reverts commit 08a69d0b2b09cfa591c362f49845a989a3cf7018.

--- sync_common_files.sh
+++ sync_common_files.sh
@@ -1,20 +1,20 @@
 #!/bin/bash
 
-ROOT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
-COMMON_DIR="$ROOT_DIR/common"
-IOS_DIR="$ROOT_DIR/ios"
-MACOS_DIR="$ROOT_DIR/macos"
-IOS_SPM_DIR="$IOS_DIR/device_calendar/Sources/device_calendar"
-MACOS_SPM_DIR="$MACOS_DIR/device_calendar/Sources/device_calendar"
+# Directories
+COMMON_DIR="../common"
+IOS_DIR="./ios"
+MACOS_DIR="./macos"
 
-mkdir -p "$IOS_SPM_DIR" "$MACOS_SPM_DIR"
-
-for file in "$COMMON_DIR"/*; do
-  filename="$(basename "$file")"
-  ln -sf "../common/$filename" "$IOS_DIR/$filename"
-  ln -sf "../common/$filename" "$MACOS_DIR/$filename"
-  cp "$file" "$IOS_SPM_DIR/"
-  cp "$file" "$MACOS_SPM_DIR/"
+# Create symlinks for iOS
+cd "$IOS_DIR"
+for file in $(ls -A $COMMON_DIR); do
+  ln -sf "$COMMON_DIR/$file" .
 done
 
-echo "Symlinks and Swift Package Manager sources updated for iOS and macOS."
+# Create symlinks for macOS
+cd "../$MACOS_DIR"
+for file in $(ls -A $COMMON_DIR); do
+  ln -sf "$COMMON_DIR/$file" .
+done
+
+echo "Symlinks updated for iOS and macOS."
